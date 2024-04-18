203105b91bed091cdb9f724b2231ef0ac11f7fb0
KonstantinovAleks <knstntnvssh@list.ru>
2024-04-18 21:26:40 +0300

    video7_git

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
-# video6
+# video7
 
 ### Description of the project
     Проект сделал по видео:
@@ -6,8 +6,10 @@
         Alishev -> Spring Mvc -> "3. Spring MVC. Конфигурация с помощью Java кода."
         Alishev -> Spring Mvc -> "4. Spring MVC. Контроллеры. Аннотация @Controller."
         Alishev -> Spring Mvc -> "6. Spring MVC. Параметры GET запроса. Аннотация @RequestParam."
+        Alishev -> Spring Mvc -> "7. Spring MVC. Модель. Передача данных от контроллера к представлению."
 
 ### Entry point
     http://localhost:8080/first/hello
     http://localhost:8080/first/goodbye
     http://localhost:8080/exit
+    http://localhost:8080/first/calculator
--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +14,10 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-
-        System.out.println("Hello, " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        // System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "first/hello";
     }
@@ -23,5 +25,36 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) int a,
+                             @RequestParam(value = "b", required = false) int b,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model){
+
+        int res;
+
+        switch (action) {
+            case "multiplication" :
+                res = a * b;
+                break;
+            case "addition" :
+                res = a + b;
+                break;
+            case "subtraction" :
+                res = a - b;
+                break;
+            case "division" :
+                res = a / b;
+                break;
+            default:
+                res = 0;
+                break;
+        }
+
+        model.addAttribute("mes", "Your result - " + res);
+
+        return "first/calculator";
     }
 }
